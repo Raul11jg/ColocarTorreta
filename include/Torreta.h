a8528a152912eb754fdae972b33b9424630b7bdb
Raul <raul11jg@gmail.com>
2019-03-12 10:50:17 +0100

    Torreta.cpp

--- include/Torreta.h
+++ include/Torreta.h
@@ -2,42 +2,20 @@
 #define TORRETA_H
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
 using namespace std;
 
 class Torreta
 {
     public:
-        Torreta(std::string imgDirectory){
-
-        if (!tex.loadFromFile(imgDirectory))
-        {
-            std::cerr << "Error cargando la imagen ballesta.png";
-            exit(0);
-        }
-        sprite.setTexture(tex);
-        //Coloco el origen del sprite en la mitad de la imagen, que tiene 150ancho 120alto. inicialmente el origen se pone en 0,0, top left.
-        sprite.setOrigin(sf::Vector2f(256, 256));
-
-        }
-        int getX(){
-            return sprite.getPosition().x;
-        }
-        int getY(){
-            return sprite.getPosition().y;
-        }
-        void setPos(sf::Vector2f newPos){
-            sprite.setPosition(newPos);
-        }
-        void draw(sf::RenderWindow &Window){
-            Window.draw(sprite);
-        }
-
-    protected:
+        Torreta(std::string imgDirectory);
+        int getX();
+        int getY();
+        void setPos(sf::Vector2f newPos);
+        void draw(sf::RenderWindow &Window);
+        ~Torreta();
     private:
-        sf::Texture tex;
-        sf::Sprite sprite;
-
+        sf::Texture *torretaTex;
+        sf::Sprite *torretaSprite;
 };
 
 #endif // TORRETA_H
--- src/Torreta.cpp
+++ src/Torreta.cpp
@@ -1,1 +1,32 @@
+#include "Torreta.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
+Torreta::Torreta(std::string imgDirectory)
+{
+    if(!torretaTex->loadFromFile(imgDirectory)){
+        std::cerr <<"Error al cargar la imagen";
+        exit(0);
+    }
+    torretaSprite->setTexture(*torretaTex);
+    torretaSprite->setOrigin(sf::Vector2f(256,256));
+    torretaSprite->setScale(0.05f,0.05f);
+}
+int Torreta::getX(){
+    return torretaSprite->getPosition().x;
+}
+int Torreta::getY(){
+    return torretaSprite->getPosition().y;
+}
+void Torreta::draw(sf::RenderWindow &Window){
+    Window.draw(*torretaSprite);
+}
+void Torreta::setPos(sf::Vector2f newPos){
+    torretaSprite->setPosition(newPos);
+}
+Torreta::~Torreta()
+{
+    //dtor
+
+}
